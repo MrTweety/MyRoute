04ebca6a74f9659ea512fe0aa4f397043e4680a7
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-10-04 17:50:44 +0200

    struct application folders

---
+++ app/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAsKc5HF0cvHdbPPiTsM6Bn66rWMp-XLnM",
+    authDomain: "gpsproject-47e03.firebaseapp.com",
+    databaseURL: "https://gpsproject-47e03.firebaseio.com",
+    projectId: "gpsproject-47e03",
+    storageBucket: "gpsproject-47e03.appspot.com",
+    messagingSenderId: "173882404308"
+  };
