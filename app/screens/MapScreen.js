51f41c7a01f73a1fd55b69848a49142a184a4912
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-11-13 23:14:07 +0100

    map v1

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,24 +1,237 @@
-import React, { Component } from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import React, { Component, Fragment } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  Animated
+} from "react-native";
 
-class MapScreen extends Component {
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+
+import route from "./cord";
+
+const { width, height } = Dimensions.get("window");
+const imgSrc = require("../assets/imgMock/3.jpg");
+console.log("MG-log: route", route.coords[1]);
+console.log("MG-log: route length", route.coords.length);
+
+export default class MapScreen extends Component {
+  animatedValue = new Animated.Value(0);
+
+  state = { liked: false };
+
+  toggleLike = () => {
+    this.setState(state => {
+      const newLiked = !state.liked;
+      return { liked: newLiked };
+    });
+    // this.showImage();
+  };
+
+  showImage = () => {
+    this.toggleLike();
+    Animated.sequence([
+      // Animated.spring(this.animatedValue, {
+      //   toValue: 1,
+      //   speed: 20,
+      //   // useNativeDriver: true
+      // }),
+      // Animated.spring(this.animatedValue, {
+      //   toValue: 0,
+      //   speed: 20
+      //   // userNativeDriver: true
+      // })
+
+      Animated.timing(this.animatedValue, {
+        toValue: 1,
+        duration: 500
+        // useNativeDriver: true
+      }),
+      Animated.timing(this.animatedValue, {
+        toValue: 0,
+        duration: 500,
+        delay: 1000
+        // userNativeDriver: true
+      })
+    ]).start(() => {
+      console.log("end animation");
+      this.toggleLike();
+    });
+  };
+
+  renderOverlay = () => {
+    const imageStyles = {
+      opacity: this.animatedValue,
+      transform: [
+        {
+          scale: this.animatedValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0.7, 1]
+          })
+        }
+      ]
+    };
+
+    //mapa is not clickable
+    const bgColor = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["rgba(255,255,255,0)", "rgba(255,255,255,0.8)"]
+    });
+
+    const animatedStyle = {
+      backgroundColor: bgColor
+      // opacity: this.animatedValue,
+    };
+
+    return (
+      <Animated.View style={[styles.overlay, animatedStyle]}>
+        {/* <Image
+          source={require("../assets/imgMock/1.jpg")}
+          // style={styles.overlayHeart}
+        /> */}
+        <Animated.View style={{ ...imageStyles }}>
+          <Image
+            style={{ width: width }}
+            // style={{ flex: 1 }}
+            width={width}
+            height={width}
+            resizeMode="contain"
+            source={imgSrc}
+          />
+        </Animated.View>
+      </Animated.View>
+    );
+  };
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      >
-        <Text style={{ color: "black" }}>This is the MapScreen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            ...route.coords[parseInt(route.coords.length / 2)],
+            latitudeDelta: 0.047 / 4,
+            longitudeDelta: 0.017 / 4
+          }}
+          provider="google"
+          liteMode
+        >
+          <MapView.Circle
+            center={route.coords[1]}
+            radius={20}
+            strokeColor={"#484848"}
+            strokeWidth={5}
+            fillColor={"#fff"}
+            zIndex={1}
+          />
+          {/* Destination Circle */}
+          <MapView.Circle
+            center={route.coords[route.coords.length - 1]}
+            radius={20}
+            strokeColor={"#484848"}
+            strokeWidth={5}
+            fillColor={"#fff"}
+            zIndex={1}
+          />
+          {/* Animating polyline */}
+          {/* <AnimatingPolylineComponent Direction={route.coords.reverse()} /> */}
+          <AnimatingPolylineComponent
+            Direction={route.coords}
+            toggleLike={this.showImage}
+            isPause={this.state.liked}
+          />
+
+          <MapView.Polyline
+            coordinates={route.coords}
+            strokeWidth={2}
+            strokeColor={"#666"}
+          />
+
+          <MapView.Marker
+            coordinate={route.coords[10]}
+            title="hfhfgh"
+            description="{marker.description}"
+          />
+        </MapView>
+        {this.renderOverlay()}
       </View>
     );
   }
 }
 
-export default MapScreen;
+class AnimatingPolylineComponent extends Component {
+  state = {
+    polylinePath: this.props.Direction, //route.coords.reverse()
+    polylinePath2: []
+  };
+
+  componentDidMount() {
+    this.animatePolyline();
+  }
+
+  animatePolyline = () => {
+    this.interval = setInterval(() => this.animatePolylineStart(), 70);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  animatePolylineStart = () => {
+    if (!this.props.isPause) {
+      if (this.state.polylinePath.length <= this.props.Direction.length) {
+        const Direction = this.props.Direction;
+
+        const polylinePath = [
+          ...Direction.slice(0, this.state.polylinePath.length - 1)
+        ];
+        this.setState({ polylinePath });
+      } else {
+        this.setState({ polylinePath: [] });
+      }
+
+      if (this.state.polylinePath2.length < this.props.Direction.length - 1) {
+        const Direction = this.props.Direction;
+        const newCords = Direction[this.state.polylinePath2.length];
+        if (newCords && newCords.image) {
+          // console.log("MG-log: AnimatingPolylineComponent -> animatePolylineStart -> newCords", newCords)
+          this.props.toggleLike();
+        }
+
+        const polylinePath2 = [...this.state.polylinePath2, { ...newCords }];
+        this.setState({ polylinePath2 });
+      } else {
+        this.setState({ polylinePath2: [] });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <Fragment>
+        {this.state.polylinePath2.length > 0 && (
+          <MapView.Polyline
+            coordinates={this.state.polylinePath2}
+            //  strokeColor="#666"
+            strokeColor="white"
+            strokeWidth={9}
+          />
+        )}
+        {this.state.polylinePath2.length > 0 && (
+          <MapView.Polyline
+            coordinates={this.state.polylinePath2}
+            //  strokeColor="#666"
+            strokeColor="#484848"
+            strokeWidth={5}
+          />
+        )}
+      </Fragment>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   mytext: {
@@ -29,5 +242,14 @@ const styles = StyleSheet.create({
     color: "white",
     textShadowOffset: { width: 5, height: 5 },
     textShadowRadius: 6
+  },
+  overlay: {
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
   }
 });
