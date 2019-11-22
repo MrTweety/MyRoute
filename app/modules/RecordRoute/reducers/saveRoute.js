8af977e2db1ae1a4c5e3e5e8d15d51f440cded84
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-11-22 15:10:39 +0100

    feat(callApi): makeContentReducer [#14]

--- app/modules/RecordRoute/reducers/saveRoute.js
+++ app/modules/RecordRoute/reducers/saveRoute.js
@@ -1,17 +1,11 @@
 import {
   SAVE_ROUTE_REQUEST,
   SAVE_ROUTE_SUCCESS,
-  SAVE_ROUTE_FAILURE
+  SAVE_ROUTE_FAILURE,
+  stateKey
 } from "../actions/saveRoute";
+import { makeContentReducer } from "../../exemplar/reducers/makeContentReducer";
 
-export default saveRouteReducer = (state = {}, action) => {
-  switch (action.type) {
-    case SAVE_ROUTE_REQUEST:
-    case SAVE_ROUTE_SUCCESS:
-    case SAVE_ROUTE_FAILURE: {
-      return { ...action };
-    }
-    default:
-      return state;
-  }
-};
+const types = [SAVE_ROUTE_REQUEST, SAVE_ROUTE_SUCCESS, SAVE_ROUTE_FAILURE];
+
+export default saveRouteReducer = makeContentReducer(types, stateKey);
