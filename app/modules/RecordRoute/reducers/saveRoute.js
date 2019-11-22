457cdd1be96c4b12b34014de21b8d389686db1d3
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-11-22 15:14:22 +0100

    modules/_common

--- app/modules/RecordRoute/reducers/saveRoute.js
+++ app/modules/RecordRoute/reducers/saveRoute.js
@@ -4,7 +4,7 @@ import {
   SAVE_ROUTE_FAILURE,
   stateKey
 } from "../actions/saveRoute";
-import { makeContentReducer } from "../../exemplar/reducers/makeContentReducer";
+import { makeContentReducer } from "../../_common/reducers/makeContentReducer";
 
 const types = [SAVE_ROUTE_REQUEST, SAVE_ROUTE_SUCCESS, SAVE_ROUTE_FAILURE];
 
--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { withTranslation } from "react-i18next";
 
-import Exemplar from "../modules/exemplar/container/Exemplar";
-
+import Exemplar from "../modules/_common/container/Exemplar";
 class SearchScreen extends Component {
   componentDidMount() {
     this.updateTitle();
