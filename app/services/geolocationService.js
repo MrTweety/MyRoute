40449d3d34409014b3cc24c5676b4859667392d6
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-12-11 21:28:33 +0100

    geolocationService

---
+++ app/services/geolocationService.js
@@ -0,0 +1,45 @@
+class GeolocationService {
+  API_URL = "https://nominatim.openstreetmap.org/";
+  format = "jsonv2";
+  zoom = 18;
+
+  constructor(config) {}
+
+  async fetchInfo(location) {
+    const { latitude, longitude } = location;
+    const { API_URL, format, zoom } = this;
+
+    const url = `${API_URL}reverse?format=${format}&lat=${latitude}&lon=${longitude}&zoom=${zoom}&addressdetails=1`;
+
+    const rawResponse = await fetch(url, {
+      headers: {
+        "User-Agent": "Web/2.0"
+      }
+    });
+    return await rawResponse.json();
+  }
+
+  async fetchNameInfo(location) {
+    const { latitude, longitude } = location;
+    const { API_URL, format, zoom } = this;
+
+    const url = `${API_URL}reverse?format=${format}&lat=${latitude}&lon=${longitude}&zoom=${zoom}&addressdetails=1`;
+    const rawResponse = await fetch(url, {
+      headers: {
+        "User-Agent": "Web/2.0"
+      }
+    });
+    const response = await rawResponse.json();
+
+    if (typeof response.address.city !== "undefined") {
+      return response.address.city;
+    } else if (typeof response.address.town !== "undefined") {
+      return response.address.town;
+    } else if (typeof response.address.village !== "undefined") {
+      return response.address.village;
+    }
+    return response.address.display_name;
+  }
+}
+
+export default new GeolocationService();
