e96e39adf56cb989b8e2778cdb1c1eca51064841
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-12-18 16:36:39 +0100

    saveRoute, fix SimpleMap bug

--- app/modules/_common/components/SimpleMap.js
+++ app/modules/_common/components/SimpleMap.js
@@ -89,9 +89,9 @@ export default class SimpleMap extends Component {
     if (mapView) {
       mapView.animateCamera({
         center: {
-          ...coords[coords.length / 2 - 1]
+          ...coords[Math.trunc(coords.length / 2 - 1)]
         },
-        ...coords[coords.length / 2 - 1],
+        ...coords[Math.trunc(coords.length / 2 - 1)],
         zoom: 15
       });
     }
